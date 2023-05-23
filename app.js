5006c00b1d5415e6ed37041211c7a0af004c4313
Na Astou <naastou1@gmail.com>
2023-05-23 10:18:18 +0200

    ajout fichier js et modification css

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
+btn.addEventListener("click", function () {
+  let previousColor;
+  let newColor;
+
+  do {
+    newColor = getRandomColor();
+  } while (newColor === previousColor);
+
+  previousColor = newColor;
+
+  document.body.style.backgroundColor = colors[newColor];
+  color.textContent = colors[newColor];
+});
+function getRandomColor() {
+  return Math.floor(Math.random() * colors.length);
+}
