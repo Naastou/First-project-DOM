574601880d7435a7e87bfa8031cfb2063881b2a5
Na Astou <naastou1@gmail.com>
2023-05-23 12:32:17 +0200

    mis à jour des fichiers

--- app2.js
+++ app2.js
@@ -1,20 +1,14 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-let previousColor;
 
 btn.addEventListener("click", function () {
-  let newColor = "#";
+  let previousColor;
+  let newColor;
 
-  for (let i = 0; i < 6; i++) {
-    newColor += hex[getRandomColor()];
-  }
-  return newColor;
+  do {
+    newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  } while (newColor === previousColor);
+
+  document.body.style.backgroundColor = newColor;
+  color.textContent = newColor;
 });
-
-document.body.style.backgroundColor = newColor;
-color.textContent = newColor;
-function getRandomColor() {
-  return Math.floor(Math.random() * hex.length);
-}
